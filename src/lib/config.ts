503906184be14086a5de1bc3714f7f69c05ca1fa
mgcvale <miguelcvalealt@gmail.com>
2026-04-19 00:28:00 -0300

    chore: fix API url parsing

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,6 @@
-const apiUrl = 'http://localhost:8080';
+import { env } from '$env/dynamic/public';
+
+const apiUrl = env.PUBLIC_API_URL ?? 'https://api.mgcvale.dev';
 
 export default {
     apiUrl: apiUrl,
@@ -35,7 +37,7 @@ export default {
         restaurantReviews: (id: number) => `${apiUrl}/food/review/from_restaurant/${id}`,
         restaurantRecommendations: () => `${apiUrl}/restaurant/recommendations`,
         followedRestaurants: (offset: number = 0, count: number = 25) => `${apiUrl}/restaurant/followed?offset=${offset}&count=${count}`,
-        restaurantFollow: (id: number) => `${apiUrl}/restaurant/${id}/follow`
+        restaurantFollow: (id: number) => `${apiUrl}/restaurant/${id}/follow`,
     },
     emailRegex: "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x20\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",
     fetchTimeout: 3000,
